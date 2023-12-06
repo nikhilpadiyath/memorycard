fba591ee0c2b1173ae76670179d9402183a5c3e5
nikhil <nikhilpadiyath@gmail.com>
2023-12-06 14:52:23 +0530

    Modified for various screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
         indie: ['Indie Flower', 'cursive'],
       },
     },
+    screens: {
+      'sm': {'min': '300px', 'max': '480px'},
+     
+      'md': {'min': '480px', 'max': '700px'},
+      
+      'lg': {'min': '701px', 'max': '1020px'},
+      
+      'xl': {'min': '1021px', 'max': '1280px'},
+      
+      '2xl': {'min': '1281px'},
+    
+    },
   },
   plugins: [],
 }
